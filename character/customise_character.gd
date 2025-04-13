95b27495aa6ad45b74178b6f2ad3e6242b16f6dc
lk941 <leekee941@gmail.com>
2025-04-14 00:40:14 +0800

    colorslider UI + character pic preview

--- character/customise_character.gd
+++ character/customise_character.gd
@@ -2,22 +2,40 @@ extends Control
 
 var sprite: AnimatedSprite2D
 var hair: AnimatedSprite2D
+@onready var face_page = $background/face_page/PageControl/FacePageControl
+@onready var hair_page = $background/face_page/PageControl/HairPageControl
+@onready var tops_page = $background/face_page/PageControl/TopsPageControl
+@onready var all_pages = [$background/face_page/PageControl/FacePageControl, 
+$background/face_page/PageControl/HairPageControl,
+$background/face_page/PageControl/TopsPageControl]
 
 func _ready():
 	sprite = $background/player/CharacterBody2D/Body
 	hair = $background/player/CharacterBody2D/Hair
+	_show_page(face_page)
+	
+func _show_page(page: Control):
+	for p in all_pages:
+		p.visible = false
+	page.visible = true
 
 func _on_back_season_button_pressed():
 	get_tree().change_scene_to_file("res://select_season_screen.tscn")
 
 func _on_face_page_button_pressed():
 	$background/face_page.color = Color("#f0b9df")
+	_show_page(face_page)
+	$background/face_page/PageLabel.text = "Main Features"
 
 func _on_hair_page_button_pressed():
 	$background/face_page.color = Color("#ceaaed")
+	_show_page(hair_page)
+	$background/face_page/PageLabel.text = "Hairstyles"
 
 func _on_tops_page_button_pressed():
 	$background/face_page.color = Color("#9c8bdb")
+	_show_page(tops_page)
+	$background/face_page/PageLabel.text = "Tops | Dresses"
 
 
 func _on_avatar_left_button_pressed() -> void:
